1de27cd8f05ad62a201d20229e10f8b6dda9ca30
JungHyun <freenham@gmail.com>
2017-07-24 15:45:53 +0900

    12.3 우선순위, 순환 할당

--- thisisjava_chapter12/src/sec03/CalcThread.java
+++ thisisjava_chapter12/src/sec03/CalcThread.java
@@ -7,9 +7,14 @@ public class CalcThread extends Thread{
 		
 	@Override
 	public void run() {
+		int sum = 0;
 		for(int i = 0;i<2000000000;i++) {
-			
+			sum+=i;
+			for(int j =0;j<1;j++) {
+				
+			}
 		}
+		
 		System.out.println(getName()+" "+getPriority());
 	}
 
